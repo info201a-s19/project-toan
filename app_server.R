efd73df3488ae9832c3321f2fe0fa8e2505dabb7
Toan Vo <toanvo99@uw.edu>
2019-06-07 03:54:36 -0400

    fixed style

--- app_server.R
+++ app_server.R
@@ -4,60 +4,135 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(plotly)
+library(lintr)
 
 server <- function(input, output) {
   output$pie <- renderPlotly({
-    volcano_data_set <- read.csv("data/volcano_1980.csv", stringsAsFactors = FALSE)
-    if(input$pie_var == "Country") {
+    volcano_data_set <- read.csv("data/volcano_1980.csv",
+      stringsAsFactors = FALSE
+    )
+    if (input$pie_var == "Country") {
       volcano_data <- volcano_data_set %>%
         filter(Year == input$year_var) %>%
         group_by(Country) %>%
         summarize(Incidents = n())
-      p <- plot_ly(volcano_data, labels = ~Country, values = ~Incidents, type = 'pie', 
-                   showlegend = FALSE) %>%
-        layout(title = paste0("Proportion of ", input$pie_var, " of volcanos in ", input$year_var),
-               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+      p <- plot_ly(volcano_data,
+        labels = ~Country, values = ~Incidents, type = "pie",
+        showlegend = FALSE
+      ) %>%
+        layout(
+          title = paste0(
+            "Proportion of ", input$pie_var,
+            " of volcanos in ", input$year_var
+          ),
+          xaxis = list(
+            showgrid = FALSE, zeroline = FALSE,
+            showticklabels = FALSE
+          ),
+          yaxis = list(
+            showgrid = FALSE, zeroline = FALSE,
+            showticklabels = FALSE
+          )
+        )
       p
-    } 
-    else if(input$pie_var == "Type"){
+    }
+    else if (input$pie_var == "Type") {
       volcano_data <- volcano_data_set %>%
         filter(Year == input$year_var) %>%
         group_by(Type) %>%
         summarize(Incidents = n())
-      p <- plot_ly(volcano_data, labels = ~Type, values = ~Incidents, type = 'pie', 
-                   showlegend = FALSE) %>%
-        layout(title = paste0("Proportion of ", input$pie_var, "of volcanos in ", input$year_var),
-               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)) 
+      p <- plot_ly(volcano_data,
+        labels = ~Type, values = ~Incidents, type = "pie",
+        showlegend = FALSE
+      ) %>%
+        layout(
+          title = paste0(
+            "Proportion of ", input$pie_var,
+            "of volcanos in ", input$year_var
+          ),
+          xaxis = list(
+            showgrid = FALSE, zeroline = FALSE,
+            showticklabels = FALSE
+          )
+        )
       p
     }
-    else if(input$pie_var == "Status") {
+    else if (input$pie_var == "Status") {
       volcano_data <- volcano_data_set %>%
-        filter(Year == Status) %>%
-        group_by(input$pie_var) %>%
+        filter(Year == input$year_var) %>%
+        group_by(Status) %>%
         summarize(Incidents = n())
-      p <- plot_ly(volcano_data, labels = ~Status, values = ~Incidents, type = 'pie', 
-                   showlegend = FALSE) %>%
-        layout(title = paste0("Proportion of ", input$pie_var, " of volcanos in ", input$year_var),
-               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)) 
+      p <- plot_ly(volcano_data,
+        labels = ~Status, values = ~Incidents, type = "pie",
+        showlegend = FALSE
+      ) %>%
+        layout(
+          title = paste0(
+            "Proportion of ", input$pie_var,
+            " of volcanos in ", input$year_var
+          ),
+          xaxis = list(
+            showgrid = FALSE, zeroline = FALSE,
+            showticklabels = FALSE
+          )
+        )
       p
     }
     else {
       volcano_data <- volcano_data_set %>%
         filter(Year == input$year_var) %>%
         drop_na(input$pie_var)
+      volcano_data_na <- volcano_data_set %>%
+        filter(Year == input$year_var)
       volcano_data <- volcano_data %>%
         mutate(string = paste(
           paste0(Name, ", ", Country),
           paste0("Date: ", Month, "/", Day, "/", Year),
           sep = "<br>"
         ))
-      p <- plot_ly(volcano_data, labels = ~string, values = ~volcano_data[, input$pie_var], type = 'pie', 
-                   showlegend = FALSE) %>%
-        layout(title = paste0("Proportion of ", input$pie_var, " per incident in ", input$year_var),
-               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-      p                
+      if (is.null(dim(volcano_data$Year))) {
+        p <- plot_ly(volcano_data,
+          labels = ~string, values = ~ volcano_data[, input$pie_var],
+          type = "pie",
+          showlegend = FALSE
+        ) %>%
+          layout(
+            title = paste0(
+              "No data available for ", input$pie_var,
+              " for Year ", input$year_var
+            ),
+            xaxis = list(
+              showgrid = FALSE, zeroline = FALSE,
+              showticklabels = FALSE
+            ),
+            yaxis = list(
+              showgrid = FALSE, zeroline = FALSE,
+              showticklabels = FALSE
+            )
+          )
+      }
+      else {
+        p <- plot_ly(volcano_data,
+          labels = ~string, values = ~ volcano_data[, input$pie_var],
+          type = "pie",
+          showlegend = FALSE
+        ) %>%
+          layout(
+            title = paste0(
+              "Proportion of ", input$pie_var, " per incident in ",
+              input$year_var, "<br>Number of unrecorded incidents:"
+            ),
+            xaxis = list(
+              showgrid = FALSE, zeroline = FALSE,
+              showticklabels = FALSE
+            ),
+            yaxis = list(
+              showgrid = FALSE, zeroline = FALSE,
+              showticklabels = FALSE
+            )
+          )
+      }
+      p
     }
   })
 }
