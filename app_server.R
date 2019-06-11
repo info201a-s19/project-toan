edd54b7e1a93691220fffdee85be7680978006b5
Toan Vo <toanvo99@uw.edu>
2019-06-11 02:13:54 -0400

    finished

--- app_server.R
+++ app_server.R
@@ -1,16 +1,18 @@
 # Application server
 library(shiny)
 library(dplyr)
-library(ggplot2)
 library(tidyr)
 library(plotly)
 library(lintr)
+library(DT)
 
 server <- function(input, output) {
+  volcano_data_set <- read.csv("data/volcano_1980.csv",
+                               stringsAsFactors = FALSE)
+  #pie chart
   output$pie <- renderPlotly({
-    volcano_data_set <- read.csv("data/volcano_1980.csv",
-      stringsAsFactors = FALSE
-    )
+    #first 3 branches of if statement used due to syntax issues
+    #and different pie chart produced
     if (input$pie_var == "Country") {
       volcano_data <- volcano_data_set %>%
         filter(Year == input$year_var) %>%
@@ -78,6 +80,7 @@ server <- function(input, output) {
         )
       p
     }
+    #creates pie chart for variable not listed above
     else {
       volcano_data <- volcano_data_set %>%
         filter(Year == input$year_var) %>%
@@ -85,12 +88,14 @@ server <- function(input, output) {
       volcano_data_na <- volcano_data_set %>%
         filter(Year == input$year_var)
       num_missing <- nrow(volcano_data_na) - nrow(volcano_data)
+      #produces hover text
       volcano_data <- volcano_data %>%
         mutate(string = paste(
           paste0(Name, ", ", Country),
           paste0("Date: ", Month, "/", Day, "/", Year),
           sep = "<br>"
         ))
+      #if all incidents in selected year have unrecorded variable, print this
       if (nrow(volcano_data) == 0) {
         p <- plot_ly(volcano_data,
           labels = ~string, values = ~ volcano_data[, input$pie_var],
@@ -112,6 +117,8 @@ server <- function(input, output) {
             )
           )
       }
+      #otherwise produce pie chart, saying number of incidents
+      #with unrecorded variable
       else {
         p <- plot_ly(volcano_data,
           labels = ~string, values = ~ volcano_data[, input$pie_var],
@@ -136,5 +143,48 @@ server <- function(input, output) {
       }
       p
     }
+  })
+  #produces table for question nuumber 1 in takeaway page
+  output$table_one <- DT::renderDataTable({
+    vol_df <- volcano_data_set %>%
+      drop_na(Year) %>%
+      group_by(Country) %>%
+      summarise(Incidents = n())
+    vol_df_two <- volcano_data_set %>%
+      drop_na(TOTAL_DAMAGE_MILLIONS_DOLLARS)
+    vol_table <- vol_df_two %>%
+      group_by(Country) %>%
+      summarize(Incidents = n(), TOTAL_DAMAGE_MILLIONS = 
+                  round(sum(TOTAL_DAMAGE_MILLIONS_DOLLARS) / n(), 1)) 
+    vol_table <- left_join(vol_df, vol_table, by = "Country")
+    vol_table$TOTAL_DAMAGE_MILLIONS[is.na(vol_table$TOTAL_DAMAGE_MILLIONS)] <- "Not Available"
+    vol_table$Incidents.y[is.na(vol_table$Incidents.y)] <- 0
+    vol_table <- vol_table %>%
+      mutate("Missing incidents" = Incidents.x - Incidents.y)
+    names(vol_table)[names(vol_table) == "Incidents.y"] <-
+      "Number of Incidents with Recorded Damage in Millions of $"
+    names(vol_table)[names(vol_table) == "Incidents.x"] <-
+      "Total Number of Incidents in the past 19 years"
+    names(vol_table)[names(vol_table) == "TOTAL_DAMAGE_MILLIONS"] <-
+      "Total damage within the past 19 years, in millions of dollars"
+    vol_table <- vol_table[c(1, 2, 3, 5, 4)]
+    return(vol_table)
+  })
+  #table for insight 2
+  output$table_two <- DT::renderDataTable({
+    vol_df <- volcano_data_set %>%
+      drop_na(DEATHS) %>% 
+      filter(Year <= 1990) %>%
+      filter(DEATHS < 100)
+      vol_df <- vol_df[c(1, 2, 3, 6, 8, 17)]
+  })
+  
+  output$table_three <- DT::renderDataTable({
+   vol_df <- volcano_data_set %>%
+     mutate(Volcano = paste0(Name, ", ", Country)) %>%
+     filter(Year >= 1989) %>%
+     group_by(Volcano) %>%
+     summarize(Eruptions = n()) %>%
+     arrange(-Eruptions)
   })
 }
--- app_ui.R
+++ app_ui.R
@@ -1,38 +1,57 @@
 # Load in packages
-library("ggplot2")
 library("rsconnect")
 library(plotly)
 library(shiny)
+library(DT)
 
+# reads in data set
 volcano_data_set <- read.csv("data/volcano_1980.csv", stringsAsFactors = FALSE)
+# gets rid of a row with no elements filled
 volcano_data_set <- volcano_data_set[-1, ]
+# creates set of variables as options to explore
 volcano_select <- colnames(volcano_data_set[, c(8, 12, 13, 17:36)])
+# creates list of years for user to explore further
 year_select <- unique(volcano_data_set[, 1])
 
-# Application UI
+# variable used in pie chart to investigate
 var_select <- selectInput(
   "pie_var",
-  label = "Select a variable to investigate",
+  label = 
+  HTML('<p style=
+       "color:black;font-size:16px;">Select a variable to investigate:'),
   choices = volcano_select,
   selected = "Country"
 )
 
+# year used to investigate variable
 yr_select <- selectInput(
   "year_var",
-  label = "Select a year",
+  label = HTML('<p style="color:black;font-size:16px;">Select a year:'),
   choices = year_select,
   selected = "1980"
 )
 
+# combines user input menus into panel
 pie_sidebar <- sidebarPanel(
   var_select,
   yr_select
 )
 
+# description of chart and plots chart
 pie_main <- mainPanel(
+  p("The pie chart attempts to provide an insight to the proportion
+    of a certain variable for a certain year. For instance, if you selected
+    the variable \"Type\",  then the chart would show the proportion of
+    volcanos of a certain type (Stratovolcano, etc.) that erupted in that
+    year. However, if picking a variable such as number of deaths, the pie 
+    char would show the number of deaths per incident for that year in
+    proportion to the rest of the year's incidents, in addition to 
+    the number of incidents that had an unrecorded number of deaths to
+    show that there are incidents with unrecorded numbers."),
   plotlyOutput("pie")
 )
 
+# creates tab panel for pie chart
 pie_page <- tabPanel(
   "Pie Chart Exploration",
   titlePanel("Pie Chart Exploration"),
@@ -43,8 +62,94 @@ pie_page <- tabPanel(
 )
 
 
-ui <- navbarPage(
-  "Volcano Data",
-  pie_page
+# page used for project insights
+insight_panel <- mainPanel(
+#insight 1
+  h4("Question 1: What countries had the most amount of damage in 
+      millions of dollars out of all the incidents in the 21st 
+      century?"),
+  br(),
+  p("This question provides an insight to the countries most recently 
+      affected and the amount of money lost in trying to fix what has been
+      lost. This data could also be used to help better allocate resources
+      to countries who might not have resources readily available. Due to the
+      number of missing values in the dataset, a lot of the countries do not
+      appear in the table below. However, the countries that have a number 
+      on the list give an estimate as to how much relief aid might be needed.
+      An interesting observation is the Indonesia having the same number of 
+      incidents with recorded damage as the US, but those eruptions caused 
+      less damage in terms of money. That could mean those volcanos were in 
+      less populated areas.
+    "),
+  DT::dataTableOutput("table_one"),
+#insight 2
+  h4("Question 2: How many eruptions before, and including, 1990 had less 
+     than 100 people killed?"),
+  br(),
+  p("Given how many protocols and safety measures were facilitated by 
+    technology, limiting the scope to 1990 and before can give a sense 
+    as to how well people were being evacuated before the rise of a lot
+    of alarm systems, etc. The incident out of the ones listed with 
+    the highest number of deaths is Semeru in Indonesia at 70 deaths. 
+    Most of them had between 1-5 deaths, which could signify that 
+    evacuations were successful. This could be due to either a small 
+    population near the volcano, which is easier than a city, or large 
+    cities had intricate plans in case of emergencies such as this."),
+  DT::dataTableOutput("table_two"),
+#insight 3
+  h4("Question 3: Within the past 30 years, which volcanoes erupted?"),
+  br("It sort of goes without saying, but past eruptions can help 
+     predict if another will go off. I do not know the exact science for 
+     predicting these events, but the table below lists a number of of 
+     volcanos that had erupted in the past 30 years. Kilauea had 10 eruptions
+     in the past 30 years. That is on average 1 every three. This is a good 
+     indicator of the possibilty of it erupting again, but some volcanos that 
+     have laid dormant for decades can also erupt without a moment's notice."),
+  DT::dataTableOutput("table_three")
 )
 
+# main panel for intro page
+intro_panel <- mainPanel(
+  p("As a group, we were interested in the various natural phenomena that 
+     occurs around us and decided to delve deeper into an environmental 
+     structure that currently has tremendous impact on us in Seattle, 
+     Washington, and that would be Mount Rainier. Some of us have taken/are 
+     currently taking a volcanos course and we believe this would be 
+     another way to learn more about this topic, as it can greatly affect 
+     our awareness of volcanic activity. We source our volcano 
+     data (from 1980 to 2019) from the ", 
+    a("Significant Volcano Eruption Database.",
+    href =
+      "https://www.ngdc.noaa.gov/nndc/servlet/ShowDatasets?dataset=102557&search_look=50&display_look=50"
+  )),
+  img(src = "ad_233677945.jpg", width = 750, height = 500)
+)
+
+# tab for intro page
+intro_tab <- tabPanel(
+  "Introduction",
+  titlePanel("Introduction"),
+  sidebarLayout(
+    intro_panel,
+    sidebarPanel(
+      h3(HTML('<p style="color:black;">image from'), a("metro.co.uk",
+        href = "https://metro.co.uk/2017/02/03/incredible-image-captures-the-exact-second-lightning-struck-an-erupting-volcano-6425301/"
+      ))
+    )
+  )
+)
+
+questions_page <- tabPanel(
+  "Insights",
+  titlePanel("Insights"),
+  insight_panel
+)
+
+
+ui <- navbarPage(
+  "Volcano Data",
+  fluid = TRUE,
+  intro_tab,
+  pie_page,
+  questions_page
+)
